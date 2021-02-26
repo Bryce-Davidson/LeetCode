d24f4d229feb84c710f1ca16e77e27060375d2e5
Bryce-Davidson <lifebryce@gmail.com>
2021-02-26 08:05:05 -0800

    Fixed solution

--- arrayShuffle.py
+++ arrayShuffle.py
@@ -1,10 +1,10 @@
 arr = [2,5,1,3,4,7]
 
 def shuffle(nums, n):
-    res = []
-    for i, j in zip(nums[:n],nums[n:]):
-        print(res)
-        res += [i,j]
-    return res
+    new = []
 
-print(shuffle(arr, len(arr)))
+    for i, j in zip(nums[:n],nums[n:]):
+         new.extend([i,j])
+    return new
+
+print(shuffle(arr, 3))
