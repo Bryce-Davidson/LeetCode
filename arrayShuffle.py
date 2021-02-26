9325206218b5fab237dd718ba8d5906becc057d2
Bryce-Davidson <lifebryce@gmail.com>
2021-02-26 08:11:29 -0800

    Final

--- arrayShuffle.py
+++ arrayShuffle.py
@@ -1,10 +1,6 @@
 arr = [2,5,1,3,4,7]
 
 def shuffle(nums, n):
-    new = []
-
-    for i, j in zip(nums[:n],nums[n:]):
-         new.extend([i,j])
-    return new
+    return [ i for j in zip(nums[:n],nums[n:]) for i in j]
 
 print(shuffle(arr, 3))
